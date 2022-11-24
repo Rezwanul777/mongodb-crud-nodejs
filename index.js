169ba63ae2eabe1e403b01dc817f963a4f8ab9f3
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2022-11-23 23:46:17 -0800

    insertOne complete

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const client=new MongoClient(process.env.DATABASE)
 console.log('Database connected');
 
 module.exports={client}
+
+// insert single data
+
+ const insertSingleData = async()=>{
+   try {
+      const database = client.db("Company");
+      const users= database.collection("Developers")
+      const doc={
+         name:'Rezwanul Haque',
+         city:'Bogura',
+         age:'39',
+         profession:'Full Stack developer'
+      }
+      const result= await users.insertOne(doc)
+      console.log(result);
+   } catch (error) {
+      console.log(error);
+   } finally{
+      await client.close()
+   }
+}
+
+//insertSingleData()
+
+
+
